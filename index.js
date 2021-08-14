8c65b0f0b6e4cd1a533464fdf4f7c3f2893903cd
Nelsontxn <nelsontxn@gmail.com>
2021-08-14 12:57:28 +0800

    Finally fixed the add book button, moving on to other buttons

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ function addBookToLibrary() {
 
 
 //Dom Maniuplation
+const AddButtons = document.getElementById("addNewBook");
+const PopUpform = document.querySelector('.addBookForm');
+
+
+    window.onload = function(){
+        PopUpform.style.visibility = "hidden"
+    };
+
+
+AddButtons.onclick = function(){
+    if (PopUpform.style.visibility === "hidden") {
+        PopUpform.style.visibility = "visible";
+      } else {
+        PopUpform.style.visibility = "hidden";
+      }
+};
