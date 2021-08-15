18a4201b172a952dd91613bad75000d943f5ebce
Nelsontxn <nelsontxn@gmail.com>
2021-08-15 13:40:16 +0800

    added the close boxed function, working on the clear function next. Some issues but will fix soon

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+//Global Variables
+let SubmitedBookTitle = document.getElementById('BookTitle').value;
+
+let SubmittedBookAuthor = document.getElementById('BookAuthor').value;
+
+let SubmmitedBookpages = document.getElementById('NumberOfPages').value;
+
+let SubmmitedBookLanguage = document.getElementById('BookLanguage').value;
+
+let SubmittedBookStats = document.getElementById('BookStats').value;
+
+
 let myLibrary = [];
 
 function Book() {
@@ -8,21 +20,57 @@ function addBookToLibrary() {
   // do stuff here
 }
 
+//Classes
+const toggleForm = {
+    'formtoggle': function(){
+        if (PopUpform.style.visibility === "hidden") {
+            PopUpform.style.visibility = "visible";
+          } else {
+            PopUpform.style.visibility = "hidden";
+          }
+    },
+
+    'clearForm': function(){
+        SubmitedBookTitle = '';
+        SubmittedBookAuthor= '';
+        SubmmitedBookpages = '';
+        SubmmitedBookLanguage = '';
+        SubmittedBookStats = null;
+        console.log(SubmitedBookTitle);
+    }
+}
 
 //Dom Maniuplation
-const AddButtons = document.getElementById("addNewBook");
+
+//manipulate the add button below//
+
+   const AddButtons = document.getElementById("addNewBook");
 const PopUpform = document.querySelector('.addBookForm');
 
+window.onload = toggleForm.formtoggle;
 
-    window.onload = function(){
-        PopUpform.style.visibility = "hidden"
-    };
+AddButtons.onclick = toggleForm.formtoggle
 
 
-AddButtons.onclick = function(){
-    if (PopUpform.style.visibility === "hidden") {
-        PopUpform.style.visibility = "visible";
-      } else {
-        PopUpform.style.visibility = "hidden";
-      }
-};
+//manipulate the x button//
+
+const closebuttonicon = document.querySelector('.closeIcon')
+
+closebuttonicon.onclick =  toggleForm.formtoggle;
+
+
+//Manipulate the Submit button
+
+const SubmitBookicon =document.querySelector('.addBook');
+
+
+SubmitBookicon.onclick  = function(){
+    console.log(document.getElementById('BookStats'));
+}
+
+
+//Manipulate Clear button
+
+const ClearButtonIcon =document.querySelector('.clear');
+
+ClearButtonIcon.onclick = toggleForm.clearForm;
