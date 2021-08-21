a37bebd0fc7d63d0a7ace13d9f559704561d5efa
Nelsontxn <nelsontxn@gmail.com>
2021-08-21 13:49:31 +0800

    Finally got the clear function working as intended, will try to experiement with the div creation with DOM soon.

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 //Global Variables
-let SubmitedBookTitle = document.getElementById('BookTitle').value;
+let SubmitedBookTitle = document.getElementById("BookTitle").value;
 
-let SubmittedBookAuthor = document.getElementById('BookAuthor').value;
+let SubmittedBookAuthor = document.getElementById("BookAuthor").value;
 
-let SubmmitedBookpages = document.getElementById('NumberOfPages').value;
+let SubmmitedBookpages = document.getElementById("NumberOfPages").value;
 
-let SubmmitedBookLanguage = document.getElementById('BookLanguage').value;
+let SubmmitedBookLanguage = document.getElementById("BookLanguage").value;
 
-let SubmittedBookStats = document.getElementById('BookStats').value;
-
+let SubmittedBookStats = document.getElementById("BookStats").value;
 
 let myLibrary = [];
 
@@ -22,55 +21,63 @@ function addBookToLibrary() {
 
 //Classes
 const toggleForm = {
-    'formtoggle': function(){
-        if (PopUpform.style.visibility === "hidden") {
-            PopUpform.style.visibility = "visible";
-          } else {
-            PopUpform.style.visibility = "hidden";
-          }
-    },
-
-    'clearForm': function(){
-        SubmitedBookTitle = '';
-        SubmittedBookAuthor= '';
-        SubmmitedBookpages = '';
-        SubmmitedBookLanguage = '';
-        SubmittedBookStats = null;
-        console.log(SubmitedBookTitle);
+  formtoggle: function () {
+    if (PopUpform.style.visibility === "hidden") {
+      PopUpform.style.visibility = "visible";
+    } else {
+      PopUpform.style.visibility = "hidden";
     }
-}
+  },
+
+  clearForm: function () {
+    document.getElementById("BookTitle").value = "";
+    document.getElementById("BookAuthor").value = "";
+    document.getElementById("NumberOfPages").value = "";
+    document.getElementById("BookLanguage").value = "";
+    document.getElementById("BookStats").value = null;
+    console.log(document.getElementById("BookTitle").value);
+  }
+};
 
 //Dom Maniuplation
 
 //manipulate the add button below//
 
-   const AddButtons = document.getElementById("addNewBook");
-const PopUpform = document.querySelector('.addBookForm');
+const AddButtons = document.getElementById("addNewBook");
+const PopUpform = document.querySelector(".addBookForm");
 
 window.onload = toggleForm.formtoggle;
 
-AddButtons.onclick = toggleForm.formtoggle
-
+AddButtons.onclick = toggleForm.formtoggle;
 
 //manipulate the x button//
 
-const closebuttonicon = document.querySelector('.closeIcon')
+const closebuttonicon = document.querySelector(".closeIcon");
 
-closebuttonicon.onclick =  toggleForm.formtoggle;
-
+closebuttonicon.onclick = toggleForm.formtoggle;
 
 //Manipulate the Submit button
 
-const SubmitBookicon =document.querySelector('.addBook');
+const SubmitBookicon = document.querySelector(".addBook");
 
-
-SubmitBookicon.onclick  = function(){
-    console.log(document.getElementById('BookStats'));
-}
-
+SubmitBookicon.onclick = function () {
+  console.log(document.getElementById("BookStats"));
+};
 
 //Manipulate Clear button
 
-const ClearButtonIcon =document.querySelector('.clear');
+const ClearButtonIcon = document.querySelector(".clear");
 
-ClearButtonIcon.onclick = toggleForm.clearForm;
+function updateInputValues() {
+    document.getElementById("BookTitle").value = "";
+    document.getElementById("BookAuthor").value = "";
+    document.getElementById("NumberOfPages").value = "";
+    document.getElementById("BookLanguage").value = "";
+    document.getElementById("BookStats").value = null;
+    console.log(document.getElementById("BookTitle").value);
+  }
+
+ClearButtonIcon.addEventListener("click", () => {
+  console.log(document.getElementById("BookTitle").value);
+  updateInputValues() 
+});
