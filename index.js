1bd7e94ebdc8fe90e13b95cec46f442652f7629e
Nelsontxn <nelsontxn@gmail.com>
2021-11-07 17:17:48 +0800

    facing problems with the local storage, finding a fix next

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 
 let myLibrary = [];
 
-function Book() {
+class Book {
   // the constructor...
-  constructor(title, author, pages, isRead) {
+  constructor(title, author, pages, language, isRead){
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.language = language;
     this.isRead = isRead;
   }
 }
@@ -17,12 +18,35 @@ function addBookToLibrary() {
   const title = document.querySelector('#BookTitle').value;
   const author = document.querySelector('#BookAuthor').value;
   const pages = document.querySelector('#NumberOfPages').value;
-  const isRead = document.querySelector('#BookLanguage').value;
+  const language = document.querySelector('#BookLanguage').value;
+  const isRead = document.querySelector('#BookStats').value;
 
-  if(title == '' || author == '' || pages == '' || isRead == ''){
+  const book = Book(title, author, pages, language, isRead);
+  //add book to Ui
 
-  }
+  //store book
 }
+
+//Store the books on local storage
+class Store {
+  static getBooks(){
+    if(localStorage.getItem('myLibrary') !== null){
+      myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+    }
+
+    return myLibrary;
+  }
+
+  static addBook(book){
+    const books = Store.getBooks();
+
+    books.push(book);
+    
+    localStorage.setItem('myLibrary',JSON.stringify(books));
+  }
+
+}
+
 
 //Classes
 const toggleForm = {
@@ -35,7 +59,47 @@ const toggleForm = {
   }
 };
 
+
+class UI{
+  static displayBooks(){
+    const books = Store.getBooks();
+    books.forEach((book) => UI.addBookToList(book));
+
+  }
+
+  static addBookToList(book){
+    const list =document.querySelector('.main');
+
+    const IndivDiv = document.createElement('div');
+
+    IndivDiv.innerHTML = `
+    <div class = 'displaybooktitle'>Book Title: ${book.title} <div>
+    <div class="displayBookAuthor">Book Author: ${book.author}</div>
+    <div class="DisplayNumberofPages">No. of Pages:${book.pages}</div>
+    <div class="DisplayBookLanguage">Language: ${book.language}</div>
+    <div class="DisplayBookStatus">Book Status: ${book.isRead}</div>
+    <div><href="x" class= "btn delete">X</a><div>
+    `;
+
+    list.appendChild(IndivDiv)
+  }
+  static deleteBook(el){
+    if(el.classlist.contains('delete')){
+      el.parentElement.parentElement.remove();
+    }
+  }
+
+  static clearFields(){
+    document.getElementById("BookTitle").value = "";
+    document.getElementById("BookAuthor").value = "";
+    document.getElementById("NumberOfPages").value = "";
+    document.getElementById("BookLanguage").value = "";
+    document.getElementById("BookStats").value = null;
+  }
+}
+
 //Dom Maniuplation
+document.addEventListener('DomContentLoaded',UI.displayBooks);
 
 //manipulate the add button below//
 
@@ -60,6 +124,21 @@ const SubmitBookicon = document.querySelector(".addBook");
 
 SubmitBookicon.onclick = function () {
   console.log(document.getElementById("BookStats"));
+  const title = document.querySelector('#BookTitle').value;
+  const author = document.querySelector('#BookAuthor').value;
+  const Pages = document.querySelector('#NumberOfPages').value;
+  const BookLanguage = document.querySelector('#BookLanguage').value;
+  const isRead  = document.querySelector('#BookStats').value;
+
+  const book = new Book(title, author, Pages, BookLanguage, isRead);
+
+  UI.addBookToList(book);
+
+  Store.addBook(book);
+
+  UI.clearFields();
+
+
 };
 
 
@@ -67,16 +146,8 @@ SubmitBookicon.onclick = function () {
 
 const ClearButtonIcon = document.querySelector(".clear");
 
-function restartInputValues() {
-  document.getElementById("BookTitle").value = "";
-  document.getElementById("BookAuthor").value = "";
-  document.getElementById("NumberOfPages").value = "";
-  document.getElementById("BookLanguage").value = "";
-  document.getElementById("BookStats").value = null;
-}
-
 ClearButtonIcon.addEventListener("click", () => {;
-  restartInputValues();
+  UI.clearFields();
   }
 ); 
 
